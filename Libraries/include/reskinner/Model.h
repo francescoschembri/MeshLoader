969605a6a31e98acebea37295fbb9b46e82bb72f
Francesco Schembri <francesco.schembri@studenti.unimi.it>
2021-10-03 09:38:51 +0200

    Reorganization project

---
+++ Libraries/include/reskinner/Model.h
@@ -0,0 +1,70 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <glad/glad.h> 
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <stb/stb_image.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include "Mesh.h"
+#include "Shader.h"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <vector>
+#include "assimp_glm_helpers.h"
+#include "AnimData.h"
+
+
+class Model
+{
+public:
+	// model data 
+	std::vector<Texture> textures_loaded;	// stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
+	std::vector<Mesh>    meshes;
+	std::string directory;
+	bool gammaCorrection;
+
+
+
+	// constructor, expects a filepath to a 3D model.
+	Model(std::string const& path, bool gamma = false);
+	// draws the model, and thus all its meshes
+	void Draw(Shader& shader, bool faces = true, bool lines = false);
+
+	std::map<std::string, BoneInfo> GetBoneInfoMap();
+	int& GetBoneCount();
+
+
+private:
+
+	std::map<std::string, BoneInfo> m_BoneInfoMap;
+	int m_BoneCounter = 0;
+
+	// loads a model with supported ASSIMP extensions from file and stores the resulting meshes in the meshes vector.
+	void loadModel(std::string const& path);
+	// processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
+	void processNode(aiNode* node, const aiScene* scene);
+
+	void SetVertexBoneDataToDefault(Vertex& vertex);
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+	void SetVertexBoneData(Vertex& vertex, int boneID, float weight);
+	void ExtractBoneWeightForVertices(std::vector<Vertex>& vertices, aiMesh* mesh, const aiScene* scene);
+	unsigned int TextureFromFile(const char* path, const std::string& directory, bool gamma = false);
+
+	// checks all material textures of a given type and loads the textures if they're not loaded yet.
+	// the required info is returned as a Texture struct.
+	std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
+};
+
+
+
+#endif
+
---
+++ src/Main.cpp
@@ -0,0 +1,248 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <reskinner/Shader.h>
+#include <reskinner/Camera.h>
+#include <reskinner/Model.h>
+#include <reskinner/Animator.h>
+
+#include <filesystem>
+#include <iostream>
+#include <bitset>
+
+namespace filesystem = std::filesystem;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window, Animator* animator, Animation animations[]);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// shortcuts
+std::bitset<8> keys("11111000");
+int anim_index = 0;
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Animated Mesh Loader", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    stbi_set_flip_vertically_on_load(true);
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile shaders
+    // -------------------------
+    Shader ourShader("./Shaders/animated_model_loading.vs", "./Shaders/animated_model_loading.fs");
+
+    // load models
+    // -----------
+    Model ourModel(filesystem::path("./Animations/Capoeira/Capoeira.dae").string());
+    Animation danceAnimation(filesystem::path("./Animations/Capoeira/Capoeira.dae").string(), ourModel);
+    Animation flairAnimation(filesystem::path("./Animations/Flair/Flair.dae").string(), ourModel);
+    Animation sillyAnimation(filesystem::path("./Animations/Silly Dancing/Silly Dancing.dae").string(), ourModel);
+
+    Animation animations[] = { danceAnimation, flairAnimation, sillyAnimation };
+
+
+    Animator animator(&animations[0]);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        // -----
+        processInput(window, &animator, animations);
+        if(keys[3]) animator.UpdateAnimation(deltaTime);
+
+        // render
+        // ------
+        glClearColor(1.0f, 0.5f, 0.05f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // don't forget to enable shader before setting uniforms
+        ourShader.use();
+
+        // view/projection transformations
+        glm::mat4 projection = glm::perspective(glm::radians(ZOOM), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+
+        // pass bones matrices to the shader
+        auto transforms = animator.GetFinalBoneMatrices();
+        for (int i = 0; i < transforms.size(); ++i)
+            ourShader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
+
+        // render the loaded model
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+        ourShader.setMat4("model", model);
+        ourModel.Draw(ourShader, keys[1], keys[6]);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window, Animator* animator, Animation animations[])
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+
+    //wireframe mode on/off
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_RELEASE) 
+        keys[5] = false;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS && !keys[5]) {
+        keys[5] = true;
+        keys[6] = !keys[6];
+    }
+
+    // activate/deactivate hidden line mode for wireframe
+    if (glfwGetKey(window, GLFW_KEY_H) == GLFW_RELEASE)
+        keys[0] = false;
+    if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS && !keys[0]) {
+        keys[0] = true;
+        keys[1] = !keys[1];
+    }
+
+    if (keys[2] = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
+    {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    }
+    else {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    }
+    
+    // reset camera
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
+        camera.Reset();
+    }
+
+    // pause animation
+    if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS && keys[4]) {
+        keys[3] = !keys[3];
+        keys[4] = false;
+    }
+    keys[4] = glfwGetKey(window, GLFW_KEY_P) == GLFW_RELEASE;
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS && keys[7]) {
+        keys[7] = false;
+        anim_index++;
+        animator->PlayAnimation(&animations[anim_index%3]);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_RELEASE) {
+        keys[7] = true;
+    }
+}
+
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (keys[2]) {
+        float xoffset = xpos - lastX;
+        float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+        camera.ProcessMouseMovement(xoffset, yoffset);
+    }
+    else {
+        lastX = xpos;
+        lastY = ypos;
+    }
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
