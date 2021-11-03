442d046aa00337fe2ecd04046a4b1e14d82fc35d
Francesco Schembri <francesco.schembri@studenti.unimi.it>
2021-11-03 10:12:21 +0100

    Bugfix getClosestPoint

--- Libraries/include/reskinner/Utility.h
+++ Libraries/include/reskinner/Utility.h
@@ -33,6 +33,7 @@ float magnitude(glm::vec3 v);
 float raySphereIntersection(glm::vec3 rayOrigin, glm::vec3 rayDir, glm::vec3 sphereCenter, float radius);
 IntersectionInfo rayPlaneIntersection(const glm::vec3 rayOrigin, const glm::vec3 rayDir, const Vertex& v1, const Vertex& v2, const Vertex& v3);
 int getClosestVertexIndex(const glm::vec3 point, const Mesh& m, int v1, int v2, int v3);
+int getClosestVertexIndex(const glm::vec3 point, const Mesh& m, Face& f);
 Line getClosestLineIndex(const glm::vec3 point, const Mesh& m, int v1, int v2, int v3);
 float getPointLineDist(const glm::vec3 l1, const glm::vec3 l2, const glm::vec3 point);
 //void tweaking(Vertex& v, Camera& cam, float xoffset, float yoffset);
